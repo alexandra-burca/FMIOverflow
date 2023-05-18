3fcac706ce172099960724bdffdb99c765331944
alexandra-burca <alexandra.h.burca@gmail.com>
2023-05-18 22:34:28 +0300

    update

--- stackprj/stackprj/urls.py
+++ stackprj/stackprj/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from stackusers import views as user_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('stackbase.urls')),
+    path('register',user_views.register,name="register"),
 ]
