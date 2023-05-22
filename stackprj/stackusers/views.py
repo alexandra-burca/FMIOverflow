190043a9de4a5de5dc4c3186ab6fab9db9c52524
alexandra-burca <alexandra.h.burca@gmail.com>
2023-05-22 23:19:17 +0300

    update login

--- stackprj/stackusers/views.py
+++ stackprj/stackusers/views.py
@@ -1,12 +1,43 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+# from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from .forms import RegisterForm, LoginForm
 
 
-def register(request):
-    form = UserCreationForm()
-    context = {"form" : form }
-    return render(request,"stackusers/register.html",context)
+def registerPage(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"You have created an account with the username {username}. You may login.")
+            return redirect('stackbase:home')
+    else:
+        form = RegisterForm()
+    context = {"form": form}
+    return render(request, "stackusers/register.html", context)
 
 
-def login(request):
-    return render(request,"login.html")
+def loginPage(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            # email = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            username = form.cleaned_data.get('username')
+            user = authenticate(request, username=username, password=password)
+            # user = authenticate(request, username=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('stackbase:home')
+    else:
+        form = AuthenticationForm()
+
+    context = {'form': form}
+    return render(request, 'stackusers/login.html', context)
+
+
+def profilePage(request):
+    return render(request, 'stackusers/profile.html')
