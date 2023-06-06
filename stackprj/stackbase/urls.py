c468e6ea48455a85d4865fed56781db5bd16fa4f
alexandra-burca <alexandra.h.burca@gmail.com>
2023-06-06 10:00:52 +0300

    comments

--- stackprj/stackbase/urls.py
+++ stackprj/stackbase/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('questions/new',views.QuestionCreateView.as_view(),name="question_create"),
     path('questions/<int:pk>',views.QuestionDetailView.as_view(),name="question_detail"),
     path('questions/<int:pk>/update/',views.QuestionUpdateView.as_view(),name="question_update"),
+    path('questions/<int:pk>/delete', views.QuestionDeleteView.as_view(), name="question_delete"),
     path('questions/<int:pk>/comment/',views.AddCommentView.as_view(),name="question_comment"),
 
 ]
--- stackprj/stackbase/views.py
+++ stackprj/stackbase/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView,CreateView,UpdateView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 from .models import Question, Comment
 from .forms import CommentForm
 from django.urls import reverse, reverse_lazy
@@ -31,10 +32,24 @@ class QuestionUpdateView(UpdateView):
     model = Question
     fields =['title','content']
     context_object_name = 'question'
-    #def form_valid(self,form):
-    #    form.instance.user=self.request.user
-    #    return super().form_valid(form)
-    
+    def form_valid(self, form):
+         form.instance.user = self.request.user
+         return super().form_valid(form)
+    def test_func(self):
+         questions = self.get_object()
+         if self.request.user == questions.user:
+            return True
+         return False
+class QuestionDeleteView(UserPassesTestMixin,DeleteView):
+    model = Question
+    context_object_name = 'question'
+    success_url="/"
+
+    def test_func(self):
+        questions= self.get_object()
+        if self.request.user == questions.user:
+            return True
+        return False
 class CommentDeatilView(CreateView):
     model = Comment
     form_class = CommentForm
