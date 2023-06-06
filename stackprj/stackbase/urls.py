ccf6845e4fbef22dc104205e3e02b68b84b07f8c
alexandra-burca <alexandra.h.burca@gmail.com>
2023-06-06 09:36:29 +0300

    comments

--- stackprj/stackbase/urls.py
+++ stackprj/stackbase/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('questions/',views.QuestionListView.as_view(),name="question_list"),
     path('questions/new',views.QuestionCreateView.as_view(),name="question_create"),
     path('questions/<int:pk>',views.QuestionDetailView.as_view(),name="question_detail"),
-    path('questions/<int:pk>/update',views.QuestionUpdateView.as_view(),name="question_update"),
+    path('questions/<int:pk>/update/',views.QuestionUpdateView.as_view(),name="question_update"),
+    path('questions/<int:pk>/comment/',views.AddCommentView.as_view(),name="question_comment"),
+
 ]
--- stackprj/stackbase/views.py
+++ stackprj/stackbase/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView,CreateView,UpdateView
-from .models import Question
+from .models import Question, Comment
+from .forms import CommentForm
+from django.urls import reverse, reverse_lazy
 
 def home(request):
     return render(request, 'home.html')
@@ -33,4 +35,23 @@ class QuestionUpdateView(UpdateView):
     #    form.instance.user=self.request.user
     #    return super().form_valid(form)
     
-    
+class CommentDeatilView(CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'stackbase/question_detail.html'
+
+    def form_valid(self,meta):
+        form.instance.question_id = self.kwargs['pk']
+        return super().form_valid(form)
+
+    success_url = reverse_lazy('stackbase:question_detail')
+
+class AddCommentView(CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'stackbase/question_answer.html'
+
+    def form_valid(self,form):
+        form.instance.question_id = self.kwargs['pk']
+        return super().form_valid(form)
+    success_url = reverse_lazy('stackbase:question_list')
